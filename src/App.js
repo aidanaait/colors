357ad0c3a2ab359e7cbac4c592a641bcf6cd2315
aidanaait <aidana.aitmukhanbetova@gmail.com>
2022-10-26 12:31:18 -0700

    colors

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import StarRating from "./components/starRating";
+import {useState} from "react";
+import colorsList from "./services/color-data.json";
+import ColorList from "./components/colorList";
+import AddColorForm from "./components/addColorForm";
+import {v4} from 'uuid'
+
+function App() {
+    const [colors, setColors] = useState(colorsList);
+    return (
+        <>
+        <AddColorForm 
+        onNewColor={(title,color)=>{
+            const newColors = [...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color
+            }];
+            console.log(newColors)
+            setColors(newColors);
+    
+          }}/>
+        <ColorList
+            colors={colors}
+            onRemoveColor={(id) => {
+                const newColors = colors.filter((color) => color.id != id);
+                setColors(newColors);
+                
+            }}
+            onRateStars={(rating, id) => {
+                const newColors = colors.map((color) =>
+                    color.id === id ? {...color, rating} : {...color}
+                );
+                setColors(newColors);
+            }}
+        /></>
+    );
+}
+
+export default App;
